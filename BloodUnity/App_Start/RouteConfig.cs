b0a86226ce185439bd31f5938ed1d3fdb85e6ddf
NoorRaisa <gazinoor01@gmail.com>
2024-06-11 00:07:14 +0600

    Changes to layout

--- BloodUnity/App_Start/RouteConfig.cs
+++ BloodUnity/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BloodUnity
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "MainHome", id = UrlParameter.Optional }
             );
         }
     }
